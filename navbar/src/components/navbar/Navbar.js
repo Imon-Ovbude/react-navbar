3d3175b8a72e997795f3840e3f159636050def63
Imon-Ovbude <imonhimio@gmail.com>
2021-11-22 16:12:39 -0800

    added media queries and conditionals when icons are clicked

--- navbar/src/components/navbar/Navbar.js
+++ navbar/src/components/navbar/Navbar.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { MenuList } from './MenuList';
 
 import './Navbar.css';
 
 const Navbar = () => {
+  const [clicked, setClicked] = useState(false);
+
   const menuList = MenuList.map(({ title, url }, index) => {
     return (
       <li key={index}>
@@ -15,15 +17,19 @@ const Navbar = () => {
     );
   });
 
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
   return (
     <nav>
       <div className='logo'>
         VPN<font>Lab</font>
       </div>
-      <div className='menu-icon'>
-        <i className='fa fa-bars'></i>
+      <div className='menu-icon' onClick={handleClick}>
+        <i className={clicked ? 'fas fa-times' : 'fa fa-bars'}></i>
       </div>
-      <ul className='menu-list'>{menuList}</ul>
+      <ul className={clicked ? 'menu-list' : 'menu-list close'}>{menuList}</ul>
     </nav>
   );
 };
